09109a94a18c449fc2a12018f1a058b79131cbeb
Dima <harmidonchick@gmail.com>
2020-04-07 12:53:26 +0300

    Adding new class

---
+++ src/main/java/net/proselyte/gitoverview/BaseEntity.java
@@ -0,0 +1,24 @@
+package net.proselyte.gitoverview;
+
+public class BaseEntity {
+
+    private Long id;
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public boolean isNew(){
+        return (this.id == null);
+    }
+}
